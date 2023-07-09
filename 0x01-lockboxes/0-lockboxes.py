6e54b4b0b49ea53463fb90982ee4ac08ecaf3738
Michael-Maina <michaelmaina02@gmail.com>
2023-07-10 01:24:06 +0300

    Bug fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,14 +9,15 @@ def canUnlockAll(boxes):
     given_keys = boxes[0]
     all_keys = []
     for count, key in enumerate(given_keys, 1):
-        # print(f"Initial {given_keys}")
-        # print(f"Key {key}")
         all_keys = given_keys.copy()
-        all_keys.extend(boxes[key])
+        if boxes[key] == []:
+            all_keys.append(0)
+        else:
+            all_keys.extend(boxes[key])
 
         # Removing duplicate keys
         for i in all_keys:
-            if i not in given_keys:
+            if i not in given_keys and i < len(boxes):
                 given_keys.append(i)
 
         print(f"After {given_keys} Count:{count} Boxes:{len(boxes)}")
