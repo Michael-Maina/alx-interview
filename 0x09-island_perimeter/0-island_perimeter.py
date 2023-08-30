b97fa333764e6243f81188d3aaed2fbc9fc4c0b9
Michael-Maina <michaelmaina02@gmail.com>
2023-08-30 09:35:40 +0300

    Bug fixing

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,13 +17,13 @@ def island_perimeter(grid):
                 # Left
                 if (((row - 1) >= 0) and (grid[row - 1][col] == 1)):
                     perimeter -= 1
-                # Right
-                if (((row + 1) >= 0) and (grid[row + 1][col] == 1)):
+                    # Right
+                if (((row + 1) < len(grid)) and (grid[row + 1][col] == 1)):
                     perimeter -= 1
-                # Top
+                    # Top
                 if (((col - 1) >= 0) and (grid[row][col - 1] == 1)):
                     perimeter -= 1
-                # Bottom
-                if (((col + 1) >= 0) and (grid[row][col + 1] == 1)):
+                    # Bottom
+                if (((col + 1) < len(grid[0])) and (grid[row][col + 1] == 1)):
                     perimeter -= 1
     return perimeter
